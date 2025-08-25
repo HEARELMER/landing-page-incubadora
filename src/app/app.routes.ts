80bcf69b6fa0c1af34fb7eabbb7bfe5399dc39ca
HEARELMER <hearelmer2005@gmail.com>
2025-08-25 12:04:58 -0500

    feat: add ContactsComponent with template, styles, and tests; update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { DefenseComponent } from './pages/defense/defense.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { DiagnosisComponent } from './pages/diagnosis/diagnosis.component';
+import { CasesComponent } from './pages/cases/cases.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/diagnosis', pathMatch: 'full'},
-    {path:'diagnosis', component:DiagnosisComponent },
-    {path: 'defense', component: DefenseComponent},
-    {path: 'about_us', component: AboutUsComponent},
-    {path: '**', redirectTo: '/diagnosis'}
+  { path: '', redirectTo: '/diagnosis', pathMatch: 'full' },
+  { path: 'diagnosis', component: DiagnosisComponent },
+  { path: 'defense', component: DefenseComponent },
+  { path: 'about_us', component: AboutUsComponent },
+  { path: 'cases_library', component: CasesComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: '**', redirectTo: '/diagnosis' },
 ];
