fbfb04252ae18abd983f58bcc497043004396ebb
HEARELMER <hearelmer2005@gmail.com>
2025-10-05 19:16:50 -0500

    fix: correct formatting and spacing in routing and enfoque component files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home.component'; 
-import { AboutUsComponent } from './pages/about-us/about-us.component'; 
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { CasesComponent } from './pages/cases/cases.component';
-import { ContactsComponent } from './pages/contacts/contacts.component';  
+import { ContactsComponent } from './pages/contacts/contacts.component';
 import { EnfoqueComponent } from './pages/enfoque/enfoque.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' }, 
+  { path: '', component: HomeComponent, pathMatch: 'full'  },
+  { path: 'home', component: HomeComponent },
   { path: 'enfoque', component: EnfoqueComponent },
   { path: 'casoteca', component: CasesComponent },
   { path: 'nosotros', component: AboutUsComponent },
--- src/app/pages/enfoque/enfoque.component.ts
+++ src/app/pages/enfoque/enfoque.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-enfoque',
+  selector: 'app-enfoque', 
   imports: [],
   templateUrl: './enfoque.component.html',
-  styleUrl: './enfoque.component.scss'
-})
-export class EnfoqueComponent {
+  styleUrl: './enfoque.component.scss',
 
+})
+export class EnfoqueComponent { 
 }
